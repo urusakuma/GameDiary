afcbe6b7d1aa3b2ed518b86491346c2d84568578
urusakuma <urusakuma@gmail.com>
2024-10-15 14:48:45 +0000

    update dailyReport メモリ使用量を下げた。

--- game-report/src/app/model/dailyReport.ts
+++ game-report/src/app/model/dailyReport.ts
@@ -34,12 +34,13 @@ export class DailyReport {
   set report(val: string) { this._report = val }
   set previous(val: number | undefined) {
     if ((this.day === 1 && val === undefined) || // 初日だけundefinedを許す
-      (val !== undefined && this.day > val && val > 0)) { // 前日が1未満になることはない
+      (val !== undefined && this.day > val && val > 0)) { // 前日は今日より大きくないし1未満にならない
       this._previous = val
     }
   }
   set next(val: number | undefined) {
-    if (val === undefined || this.day < val) { // 翌日のレポートは未作成か今日より日付が大きい
+    // 翌日のレポートは未作成か今日より日付が大きい
+    if (val === undefined || this.day < val) {
       this._next = val
     }
   }
@@ -58,5 +59,23 @@ export class DailyReport {
       this.report === "" &&
       (this.reportTitle === "" || this.reportTitle === this.defaultTitle)
     );
+  };
+
+  /**
+   * JSONに変換する。JSON.stringifyで自動的に呼び出される。
+   *  本来は実装しなくても自動でJSONが出来上がる。
+   * previousを取り除くために実装。これで約3%圧縮できる。
+   *  レポートの分量が少なければ最大で12.5%。
+   * defaultTitleも取り除く。Settingsから計算できるし、
+   *  途中で設定を変更した場合にデフォを変更できる。
+   * @returns {object} JSONにシリアライズされるオブジェクト。
+   */
+  toJSON = (): object => {
+    return {
+      day: this.day,
+      reportTitle: this.reportTitle,
+      report: this.report,
+      next: this.next,
+    };
   };
 }

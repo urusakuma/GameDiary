a3202e61e350f5887e4e8613a9e8c49748777329
urusakuma <urusakuma@gmail.com>
2025-03-05 09:06:11 +0000

    IDiaryImport の実装 Fixes #78

---
+++ game-diary/src/app/model/repository/diaryImport.ts
@@ -0,0 +1,15 @@
+import type { IDiaryDecompressor } from '../serialization/serializationInterface';
+import type { IDiaryImport, IDiaryService } from './diaryRepositoryInterfaces';
+import { inject, injectable } from 'tsyringe';
+@injectable()
+export class DiaryImport implements IDiaryImport {
+  constructor(
+    @inject('IDiaryService') private diaryService: IDiaryService,
+    @inject('IDiaryDecompressor') private diaryDecompressor: IDiaryDecompressor
+  ) {}
+  import(val: string): string {
+    const diary = this.diaryDecompressor.decompressDiary(val);
+    this.diaryService.addDiary(diary);
+    return diary.getSettings().storageKey;
+  }
+}

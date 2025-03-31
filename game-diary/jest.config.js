118099852aa681614c8b5d4acae3640ef4fe8eed
urusakuma <urusakuma@gmail.com>
2025-03-31 11:21:34 +0000

    IDiaryImporter の実装 Fixes #101

--- game-diary/jest.config.js
+++ game-diary/jest.config.js
@@ -11,9 +11,7 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/lib/$1',
   },
   setupFiles: ['<rootDir>/node_modules/reflect-metadata'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.json',
-    },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
   },
 };
---
+++ game-diary/src/lib/control/diaryImporter.ts
@@ -0,0 +1,33 @@
+import type { IDiaryImport } from '@/model/repository/diaryRepositoryInterfaces';
+import type { ICurrentDiaryAccessor, IDiaryImporter } from './controlInterface';
+import { inject } from 'tsyringe';
+
+export default class DiaryImporter implements IDiaryImporter {
+  constructor(
+    @inject('IDiaryImport') private diaryImport: IDiaryImport,
+    @inject('ICurrentDiaryAccessor')
+    private diaryAccessor: ICurrentDiaryAccessor
+  ) {}
+  importText(val: string): string {
+    const key = this.diaryImport.import(val);
+    this.diaryAccessor.setCurrentDiary(key);
+    return key;
+  }
+  async importFile(file: File): Promise<string> {
+    const key = new Promise<string>((resolve, reject) => {
+      try {
+        const reader = new FileReader();
+        reader.onload = () => {
+          const fileContent = reader.result as string;
+          const key = this.diaryImport.import(fileContent);
+          this.diaryAccessor.setCurrentDiary(key);
+          resolve(key);
+        };
+        reader.readAsText(file);
+      } catch (e) {
+        reject(e);
+      }
+    });
+    return key;
+  }
+}

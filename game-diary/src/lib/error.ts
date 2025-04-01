752ac2cb943fd22fe0255946b25e79945f1ecc5f
urusakuma <urusakuma@gmail.com>
2025-04-01 00:14:39 +0000

    DiaryExporter の実装 Fixes #102

--- game-diary/src/lib/error.ts
+++ game-diary/src/lib/error.ts
@@ -1,6 +1,7 @@
 export class ArgumentError extends Error {}
 export class KeyNotFoundError extends Error {}
 export class NotSupportedError extends Error {}
+export class NotFoundError extends Error {}
 export class QuotaExceededError extends Error {}
 export class KeyAlreadyExistsError extends Error {}
 export class DecompressionError extends Error {}

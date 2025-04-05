47b9e0758b67ed24685cdd8c22d4d5a84818f552
urusakuma <urusakuma@gmail.com>
2025-04-05 02:46:00 +0000

    IEditDiaryEntry の実装 Fixes #109

---
+++ game-diary/src/lib/__tests__/control/editDiaryEntry.test.ts
@@ -0,0 +1,80 @@
+import 'reflect-metadata';
+import EditDiaryEntry from '@/control/controlDiaryEntry/editDiaryEntry';
+import type { ICurrentDiaryAccessor } from '@/control/controlDiary/controlDiaryInterface';
+import type {
+  ICurrentDiaryEntryAccessor,
+  IEditDiaryEntry,
+} from '../../control/controlDiaryEntry/controlDiaryEntryInterface';
+import {
+  IDiary,
+  IDiaryEntry,
+  IDiarySettings,
+} from '@/model/diary/diaryModelInterfaces';
+
+describe('EditDiaryEntry', () => {
+  let editDiaryEntry: IEditDiaryEntry;
+  let mockDiaryEntryAccessor: jest.Mocked<ICurrentDiaryEntryAccessor>;
+  let mockDiaryAccessor: jest.Mocked<ICurrentDiaryAccessor>;
+
+  beforeEach(() => {
+    mockDiaryEntryAccessor = {
+      getCurrentDiaryEntry: jest.fn(),
+    } as unknown as jest.Mocked<ICurrentDiaryEntryAccessor>;
+
+    mockDiaryAccessor = {
+      getCurrentDiary: jest.fn(),
+    } as unknown as jest.Mocked<ICurrentDiaryAccessor>;
+
+    const mockDiaryEntry = {
+      setTitle: jest.fn(),
+      setContent: jest.fn(),
+      day: 1,
+    } as unknown as jest.Mocked<IDiaryEntry>;
+
+    const mockDiary = {
+      getSettings: jest.fn(),
+    } as unknown as jest.Mocked<IDiary>;
+
+    const mockSettings = {
+      getModifierDay: jest.fn().mockReturnValue('Default Title'),
+    } as unknown as jest.Mocked<IDiarySettings>;
+
+    mockDiaryEntryAccessor.getCurrentDiaryEntry.mockReturnValue(mockDiaryEntry);
+    mockDiaryAccessor.getCurrentDiary.mockReturnValue(mockDiary);
+    mockDiary.getSettings.mockReturnValue(mockSettings);
+
+    editDiaryEntry = new EditDiaryEntry(
+      mockDiaryEntryAccessor,
+      mockDiaryAccessor
+    );
+  });
+
+  it('should edit the title of the diary entry', () => {
+    const newTitle = 'New Title';
+    editDiaryEntry.editTitle(newTitle);
+
+    expect(
+      mockDiaryEntryAccessor.getCurrentDiaryEntry().setTitle
+    ).toHaveBeenCalledWith(newTitle);
+  });
+
+  it('should edit the content of the diary entry', () => {
+    const newContent = 'New Content';
+    editDiaryEntry.editContent(newContent);
+
+    expect(
+      mockDiaryEntryAccessor.getCurrentDiaryEntry().setContent
+    ).toHaveBeenCalledWith(newContent);
+  });
+
+  it('should clear the diary entry', () => {
+    editDiaryEntry.clear();
+
+    const mockEntry = mockDiaryEntryAccessor.getCurrentDiaryEntry();
+    const mockSettings = mockDiaryAccessor.getCurrentDiary().getSettings();
+
+    expect(mockSettings.getModifierDay).toHaveBeenCalledWith(mockEntry.day);
+    expect(mockEntry.setTitle).toHaveBeenCalledWith('Default Title');
+    expect(mockEntry.setContent).toHaveBeenCalledWith('');
+  });
+});

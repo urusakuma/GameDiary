edff7b973fad658780687930fe431400f50f34dc
urusakuma <urusakuma@gmail.com>
2025-10-25 08:30:23 +0000

    fix 不要なファイルの削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import 'reflect-metadata';
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
 import '../lib/container/di_diary';
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -27,7 +16,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>

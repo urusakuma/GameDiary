0bb78a859ff13ef200c1f849c55e222c24f36982
urusakuma <urusakuma@gmail.com>
2025-12-11 09:53:32 +0000

    feat: DiarySettingsのテストを改善
    - DiarySettingsクラスのテストを整理し、依存性注入のテストを追加しました。
    - setDiaryNameおよびgetDiaryNameのテストを追加しました。
    - setModifierのテストを追加しました。
    - updateDayIntervalのテストを追加し、値が1未満の場合の処理を確認しました。
    - updateCycleLengthのテストを追加しました。
    - setModifierUnitのテストを追加しました。
    - getNextDayおよびgetModifierDayのテストを追加し、正しい動作を確認しました。

--- src/lib/__tests__/model/diary/diarySettings.test.ts
+++ src/lib/__tests__/model/diary/diarySettings.test.ts
@@ -1,165 +1,123 @@
-import { MockDayModifier } from '@/__tests__/__mocks__/mockDayModifier';
 import DairySettingsConstant from '@/dairySettingsConstant';
 import {
   IDayModifier,
   IDiarySettings,
-  NewDiarySettingsFactory,
-  UseExistingDataDiarySettingsFactory,
 } from '@/model/diary/diaryModelInterfaces';
 import DiarySettings from '@/model/diary/diarySettings';
 import { container } from 'tsyringe';
+import '@/container/di_diary';
 
 describe('DairySettings class tests', () => {
-  beforeAll(() => {
-    container.register<IDayModifier>('IDayModifier', {
-      useClass: MockDayModifier,
-    });
-    container.register<string>('DIARY_NAME', {
-      useValue: DairySettingsConstant.DEFAULT_DIARY_NAME,
-    });
-    container.register<number>('DAY_INTERVAL', {
-      useValue: DairySettingsConstant.DEFAULT_DAY_INTERVAL,
-    });
-    container.register<string>('STORAGE_KEY', {
-      useFactory: () => crypto.randomUUID(),
-    });
-    container.register<number>('VERSION', {
-      useValue: DairySettingsConstant.CURRENT_VERSION,
-    });
-    container.register<IDiarySettings>('InitDiarySettings', {
-      useClass: DiarySettings,
-    });
-    container.register<UseExistingDataDiarySettingsFactory>(
-      'UseExistingDataDiarySettingsFactory',
-      {
-        useFactory:
-          () =>
-          (
-            dayModifier: IDayModifier,
-            diaryName: string,
-            dayInterval: number,
-            storageKey: string,
-            version: number
-          ) =>
-            new DiarySettings(
-              dayModifier,
-              diaryName,
-              dayInterval,
-              storageKey,
-              version
-            ),
-      }
-    );
+  let modifier: IDayModifier;
+  let settings: IDiarySettings;
+  beforeEach(() => {
+    modifier = {
+      setModifier: jest.fn(),
+      getModifier: jest.fn().mockReturnValue('modifier'),
+      updateCycleLength: jest.fn(),
+      getCycleLength: jest.fn().mockReturnValue(10),
+      getUnit: jest.fn().mockReturnValue('unit'),
+      updateUnit: jest.fn(),
+      modifyDay: jest.fn(),
+    } as unknown as IDayModifier;
+    settings = new DiarySettings(modifier, 'name', 1, 'storageKey', 1);
   });
-  test('init test', () => {
-    const settings = container.resolve<IDiarySettings>('InitDiarySettings');
-    // デフォルトの確認
-    expect(settings.getDiaryName()).toBe(
-      DairySettingsConstant.DEFAULT_DIARY_NAME
-    );
-    expect(settings.version).toBe(DairySettingsConstant.CURRENT_VERSION);
-    expect(settings.getCycleLength()).toBe(
-      DairySettingsConstant.DEFAULT_CYCLE_LENGTH
-    );
-    expect(settings.getDayInterval()).toBe(
-      DairySettingsConstant.DEFAULT_DAY_INTERVAL
-    );
-    expect(settings.getModifier()).toBe(
-      DairySettingsConstant.DEFAULT_DAY_MODIFIER
-    );
-    expect(settings.getNextDay(1)).toBe(
-      1 + DairySettingsConstant.DEFAULT_DAY_INTERVAL
-    );
-    expect(settings.getModifierDay(1)).toBe(
-      String(1) + DairySettingsConstant.DEFAULT_DAY_MODIFIER
-    );
-    for (let i = 0; i < 3; i++) {
-      expect(settings.getModifierUnit(i)).toBeUndefined();
-    }
-  });
-  test('set and update', () => {
-    const settings = container.resolve<IDiarySettings>('InitDiarySettings');
-    settings.setDiaryName('テストゲーム');
-    expect(settings.getDiaryName()).toBe('テストゲーム');
-    settings.setModifier('サイクル');
-    expect(settings.getModifier()).toBe('サイクル');
-    settings.updateCycleLength(15);
-    expect(settings.getCycleLength()).toBe(15);
-    settings.updateDayInterval(3);
-    expect(settings.getDayInterval()).toBe(3);
-    settings.updateModifierUnit('test', 0);
-    for (let i = 0; i < 4; i++) {
-      settings.updateModifierUnit('test' + String(i), i);
-      expect(settings.getModifierUnit(i)).toBe('test' + String(i));
-    }
-    // 不正な値が入ったときは1にする。
-    settings.updateDayInterval(0);
-    expect(settings.getDayInterval()).toBe(1);
-    // 小数点は切り捨てる
-    settings.updateDayInterval(5.999);
-    expect(settings.getDayInterval()).toBe(5);
-
-    // DairySettingsは受け取った値がDayModifierに渡される場合は値の検証をしない。
-    settings.updateModifierUnit('out of index', 5);
-    expect(settings.getModifierUnit(5)).toBe('out of index');
-    settings.updateCycleLength(-1);
-    expect(settings.getCycleLength()).toBe(-1);
-  });
-  test('test getNextDay', () => {
-    const settings = container.resolve<IDiarySettings>('InitDiarySettings');
-    settings.updateDayInterval(1);
-    expect(settings.getNextDay(1)).toBe(2);
-    settings.updateDayInterval(3);
-    expect(settings.getNextDay(10)).toBe(13);
-    // 不正な値なら1日目の次の日を取得する。
-    expect(settings.getNextDay(0)).toBe(4);
-    // 小数は切り捨てる。
-    expect(settings.getNextDay(2.999)).toBe(5);
-    expect(settings.getNextDay(0.999)).toBe(4);
-  });
-  test('test getModifierDay', () => {
-    const settings = container.resolve<IDiarySettings>('InitDiarySettings');
-    settings.setModifier('日目');
-    expect(settings.getModifierDay(1)).toBe('1日目');
-    settings.setModifier('サイクル');
-    expect(settings.getModifierDay(10)).toBe('10サイクル');
-    // 不正な値なら1日目を修飾した文字列を受け取る
-    expect(settings.getModifierDay(0)).toBe('1サイクル');
-    // 小数は切り捨てる
-    expect(settings.getModifierDay(2.999)).toBe('2サイクル');
-    expect(settings.getModifierDay(0.999)).toBe('1サイクル');
-  });
-  test('two DiarySettings have different storage key', () => {
-    const settingsArr = [
-      container.resolve<IDiarySettings>('InitDiarySettings'),
-      container.resolve<IDiarySettings>('InitDiarySettings'),
-    ];
-    expect(
-      settingsArr[0].storageKey !== settingsArr[1].storageKey
-    ).toBeTruthy();
-  });
-
-  test('make to use container', () => {
-    const dayModifier = container.resolve<IDayModifier>('IDayModifier');
-    const useExistingDataFactory =
-      container.resolve<UseExistingDataDiarySettingsFactory>(
-        'UseExistingDataDiarySettingsFactory'
+  describe('Dependency Injection tests', () => {
+    it('should regist container', () => {
+      const settings = container.resolve<IDiarySettings>('IDiarySettings');
+      expect(settings.getDiaryName()).toBe(
+        DairySettingsConstant.DEFAULT_DIARY_NAME
       );
-    const useExistingDataSettings = useExistingDataFactory(
-      dayModifier,
-      'test data',
-      1,
-      'bec0da1f-0053-4c59-acfb-f4a574bd8c98',
-      1
-    );
-    expect(useExistingDataSettings.version).toBe(1);
-    expect(useExistingDataSettings.storageKey).toBe(
-      'bec0da1f-0053-4c59-acfb-f4a574bd8c98'
-    );
-    expect(useExistingDataSettings.getDiaryName()).toBe('test data');
-    expect(useExistingDataSettings.getDayInterval()).toBe(1);
-    expect(useExistingDataSettings.getModifier()).toBe(
-      DairySettingsConstant.DEFAULT_DAY_MODIFIER
-    );
+      expect(settings.version).toBe(DairySettingsConstant.CURRENT_VERSION);
+      expect(settings.getCycleLength()).toBe(
+        DairySettingsConstant.DEFAULT_CYCLE_LENGTH
+      );
+      expect(settings.getDayInterval()).toBe(
+        DairySettingsConstant.DEFAULT_DAY_INTERVAL
+      );
+      expect(settings.getModifier()).toBe(
+        DairySettingsConstant.DEFAULT_DAY_MODIFIER
+      );
+    });
+  });
+  describe('setDiaryName, getDiaryName tests', () => {
+    it('should set and get diary name', () => {
+      const name = 'My Diary';
+      settings.setDiaryName(name);
+      expect(settings.getDiaryName()).toBe(name);
+    });
+  });
+  describe('setModifier tests', () => {
+    it('should set modifier via IDayModifier', () => {
+      const modStr = 'New Modifier';
+      settings.setModifier(modStr);
+      expect(modifier.setModifier).toHaveBeenCalledWith(modStr);
+    });
+  });
+
+  describe('updateDaiInterval tests', () => {
+    it('should update dayInterval to value when there is no problem', () => {
+      settings.updateDayInterval(5);
+      expect(settings.getDayInterval()).toBe(5);
+    });
+    it('should update dayInterval to 1 when provided value is less 1', () => {
+      settings.updateDayInterval(0);
+      expect(settings.getDayInterval()).toBe(1);
+    });
+    it('should truncate decimal values from dayInterval', () => {
+      settings.updateDayInterval(2.999);
+      expect(settings.getDayInterval()).toBe(2);
+      settings.updateDayInterval(0.999);
+      expect(settings.getDayInterval()).toBe(1);
+    });
+  });
+  describe('updateCycleLength tests', () => {
+    it('should update cycleLength via IDayModifier', () => {
+      const cycleLength = 5;
+      settings.updateCycleLength(cycleLength);
+      expect(modifier.updateCycleLength).toHaveBeenCalledTimes(1);
+      expect(modifier.updateCycleLength).toHaveBeenCalledWith(cycleLength);
+    });
+  });
+  describe('setModifierUnit tests', () => {
+    it('should update cycleLength via IDayModifier', () => {
+      const str = 'test' + String(0);
+      settings.updateModifierUnit(str, 0);
+      expect(modifier.updateUnit).toHaveBeenCalledTimes(1);
+      expect(modifier.updateUnit).toHaveBeenCalledWith(str, 0);
+    });
+  });
+
+  describe('getNextDay tests', () => {
+    it('should return next day correctly', () => {
+      expect(settings.getNextDay(1)).toBe(2);
+      expect(settings.getNextDay(10)).toBe(11);
+    });
+    it('should return dayInterval + 1 when provided day is less than 1', () => {
+      expect(settings.getNextDay(0)).toBe(settings.getDayInterval() + 1);
+      expect(settings.getNextDay(-5)).toBe(settings.getDayInterval() + 1);
+    });
+    it('should truncate decimal values from provided day', () => {
+      expect(settings.getNextDay(2.999)).toBe(3); // 2+1=3
+      expect(settings.getNextDay(0.999)).toBe(settings.getDayInterval() + 1);
+    });
+  });
+
+  describe('getModifierDay tests', () => {
+    it('should return modifierDay via IDayModifier', () => {
+      settings.getModifierDay(1);
+      expect(modifier.modifyDay).toHaveBeenCalledTimes(1);
+      expect(modifier.modifyDay).toHaveBeenCalledWith(1);
+    });
+    it('should return modifier 1 when value less 1', () => {
+      settings.getModifierDay(0);
+      expect(modifier.modifyDay).toHaveBeenCalledWith(1);
+    });
+    it('should truncate decimal values from provided day', () => {
+      settings.getModifierDay(2.999);
+      expect(modifier.modifyDay).toHaveBeenCalledWith(2);
+      settings.getModifierDay(0.999);
+      expect(modifier.modifyDay).toHaveBeenCalledWith(1);
+    });
   });
 });

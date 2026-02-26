4c7178dc19a9dcb2057eb571580aa1b66d144aaa
urusakuma <urusakuma@gmail.com>
2026-02-26 15:00:16 +0000

    fix: delete use `useDiaryEntriesListContext`

--- src/features/diary/components/diaryEntry/DiaryEntryStateProvider.tsx
+++ src/features/diary/components/diaryEntry/DiaryEntryStateProvider.tsx
@@ -4,7 +4,6 @@ import { container } from 'tsyringe';
 
 import ContextWrapperProps from '@shared/components/contextWrapperProps';
 import { useRefreshContext } from '@features/diary/components/app/RefreshContext';
-import { useDiaryEntriesListContext } from '@features/diary/components/diary/DiaryEntryListContext';
 import useDiaryEntryTitle from '@features/diary/hooks/editDiaryEntry/useDiaryEntryTitle';
 import useDiaryEntryContent from '@features/diary/hooks/editDiaryEntry/useDiaryEntryContent';
 import useClearDiaryEntry from '@features/diary/hooks/editDiaryEntry/useClearDiaryEntry';
@@ -33,8 +32,7 @@ export const DiaryEntryProvider = ({ children }: ContextWrapperProps) => {
   const { title, updateTitle, refreshTitle } = useDiaryEntryTitle();
   const { content, updateContent, refreshContent } = useDiaryEntryContent();
   const { clear } = useClearDiaryEntry();
-  const { entryRefresherRegister, refreshEntry } = useRefreshContext();
-  const {} = useDiaryEntriesListContext();
+  const { entryRefresherRegister, refreshAll } = useRefreshContext();
 
   const [currentDiaryEntry, setCurrentDiaryEntryAccessor] =
     useState<ICurrentDiaryEntryAccessor>();
@@ -51,7 +49,6 @@ export const DiaryEntryProvider = ({ children }: ContextWrapperProps) => {
     const unregister = entryRefresherRegister(refresh);
     return unregister;
   }, [entryRefresherRegister, refreshTitle, refreshContent]);
-  const { updateDiaryEntryTitle } = useDiaryEntriesListContext();
 
   const titleObj: DiaryEntryTitleContextType = useMemo(() => {
     return {
@@ -61,11 +58,10 @@ export const DiaryEntryProvider = ({ children }: ContextWrapperProps) => {
           return;
         }
         updateTitle(title);
-        const day = currentDiaryEntry.getCurrentDiaryEntry().day;
-        updateDiaryEntryTitle(day, title);
+        refreshAll();
       },
     };
-  }, [title, updateTitle, currentDiaryEntry, updateDiaryEntryTitle]);
+  }, [title, updateTitle, currentDiaryEntry, refreshAll]);
   const contentObj: DiaryEntryContextContextType = useMemo(() => {
     return {
       content,
@@ -79,13 +75,10 @@ export const DiaryEntryProvider = ({ children }: ContextWrapperProps) => {
           return;
         }
         clear();
-        refreshEntry();
-        const day = currentDiaryEntry.getCurrentDiaryEntry().day;
-        const title = currentDiaryEntry.getCurrentDiaryEntry().getTitle();
-        updateDiaryEntryTitle(day, title);
+        refreshAll();
       },
     };
-  }, [clear, refreshEntry, currentDiaryEntry]);
+  }, [clear, refreshAll, currentDiaryEntry]);
   return (
     <DiaryEntryTitleContext.Provider value={titleObj}>
       <DiaryEntryStateContext.Provider value={contentObj}>
